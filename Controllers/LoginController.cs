10ce0cdcad984aaa4a46d3fb722ab316a033b9a7
pavithracad01 <pavithracad01@gmail.com>
2024-03-21 17:37:49 +0530

    mvc project added

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,88 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MVCPROJECT.Models;
+using Microsoft.Data.SqlClient;
+
+namespace MVCPROJECT.Controllers;
+
+public class AdminController : Controller
+  {
+
+    SqlConnection con=new SqlConnection();
+
+    SqlCommand com=new SqlCommand();
+    
+
+    SqlDataReader? dr;
+
+
+    private readonly ILogger<AdminController> _logger;
+
+    public AdminController(ILogger<AdminController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+  
+    public IActionResult About()
+    {
+        return View();
+    }
+    public IActionResult Contact()
+    {
+        return View();
+    }
+    public IActionResult Product()
+    {
+        return View();
+    }
+    public IActionResult create()
+    {
+        return View();
+    }
+    [HttpGet]
+     public IActionResult Login()
+    {
+        return View();
+    }
+
+
+
+    void connectionstring(){
+        con.ConnectionString="data source=192.168.1.240\\SQLEXPRESS; database=CAD_OFS; User Id= CADBATCH01; Password=CAD@123pass; TrustServerCertificate=True;";
+
+    }
+    [HttpPost]
+
+    public IActionResult VerifyLogin(LoginModel lmodel){
+        connectionstring();
+        con.Open();
+        com.Connection=con;
+        com.CommandText="select * from ofs_login  where Username='"+lmodel.username+"' and password='"+lmodel.password+"' ";
+        dr=com.ExecuteReader();
+
+        if(dr.Read()){
+            con.Close();
+            return View("Success");
+
+        }
+        else{
+              con.Close();
+            return View("Error");
+
+
+        }
+       
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+ }
---
+++ Models/LoginModel.cs
@@ -0,0 +1,7 @@
+namespace MVCPROJECT.Controllers;
+
+public class LoginModel{
+    public string? username{get; set;}
+    public string? password{get; set;}
+
+}
